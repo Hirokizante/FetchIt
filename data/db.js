9288c9de4b0d7a5e564472552c7aee9434c88781
Shidonai <90672790+Hirokizante@users.noreply.github.com>
2026-04-02 09:15:54 +0800

    changed connection variables

--- data/db.js
+++ data/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Sachielthechosen06',
-  database: 'fetchit'
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306
 });
 
 // Connect to MySQL
